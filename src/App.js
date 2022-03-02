1957adf8bc93e962de3556d27d8edaa6f2955244
Dhairya Upadhyay <dhairya221b@gmail.com>
2022-03-02 17:42:51 +0530

    Cleaned up completely and made some changes to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import react from "react"
 
 function App() {
+  const [bl,setBl] = React.useState(5);
+  const [sl,setSl] = React.useState(25);
+
   return (
     <div className="App">
-      
+      <div className="head">
+        <h2>25 + 5 Clock</h2>
+      </div>
     </div>
   );
 }
